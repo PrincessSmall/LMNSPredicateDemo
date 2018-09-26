c8a1923b06cc573b31075b2cd9aa1d93f4b9e54f
李敏 <1512510162@qq.com>
2018-09-26 11:26:09 +0800

    first commit

---
+++ LMNSPredicateDemo/LMPerson.h
@@ -0,0 +1,13 @@
+//
+//  LMPerson.h
+//  LMNSPredicateDemo
+//
+//  Created by 李敏 on 2018/9/26.
+//  Copyright © 2018年 李敏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LMPerson : NSObject
+
+@end
---
+++ LMNSPredicateDemo/LMPerson.m
@@ -0,0 +1,13 @@
+//
+//  LMPerson.m
+//  LMNSPredicateDemo
+//
+//  Created by 李敏 on 2018/9/26.
+//  Copyright © 2018年 李敏. All rights reserved.
+//
+
+#import "LMPerson.h"
+
+@implementation LMPerson
+
+@end
